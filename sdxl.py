7cca18da47e33681eb4837a58ac644d5df544e09
Schmeeow <43118081+Schmeeow@users.noreply.github.com>
2024-02-05 21:27:05 +0300

    Update sdxl.py

--- sdxl.py
+++ sdxl.py
@@ -3,7 +3,7 @@ from diffusers import AutoPipelineForText2Image
 import torch
 import datetime
 
-COMMON_NEGATIVE_PROMPT = 'duplicate, blurry, disfigured, deformed, poorly drawn, extra limbs, watermark, long neck, elongated body, cropped image, deformed hands, twisted fingers, double image, malformed hands, multiple heads, extra limb, ugly, poorly drawn hands, missing limb, lousy >
+COMMON_NEGATIVE_PROMPT = 'duplicate, blurry, disfigured, deformed, poorly drawn, extra limbs, watermark, long neck, elongated body, cropped image, deformed hands, twisted fingers, double image, malformed hands, multiple heads, extra limb, ugly, poorly drawn hands, missing limb, lousy'
 
 STYLES = {'basic': { 'model':'jzli/realcartoonXL-v6',
                      'pre_prompt':'',
@@ -12,17 +12,17 @@ STYLES = {'basic': { 'model':'jzli/realcartoonXL-v6',
                    },
           'photo': { 'model':'n0madic/colossusProjectXL_v53',
                      'pre_prompt':'photo of ',
-                     'positive_expansion':'photographic, realistic, realism, photography, f/2.8, 35mm photo, highly detailed, cinematic, bokeh, intricate, elegant, sharp, fine detail, aesthetic, pretty, raw photo, photorealistic, 4K, stock photo, photograph, natural soft light, detail>
+                     'positive_expansion':'photographic, realistic, realism, photography, f/2.8, 35mm photo, highly detailed, cinematic, bokeh, intricate, elegant, sharp, fine detail, aesthetic, pretty, raw photo, photorealistic, 4K, stock photo, photograph, natural soft light, detail',
                      'negative_expansion':'art, drawing, painting, drawing, illustration, anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, impressionist, noisy, blurry'
                    },
           '3d':    { 'model': 'stablediffusionapi/protovisionxl-v3',
                      'pre_prompt':'3d render of ',
                      'positive_expansion':'3d model, video game character, volumetric light, 4k, active, dynamic, highly detailed, intricate, dramatic light, elegant, sharp, vivid colors, fine detail, fair quality, aesthetic, pretty, attractive, enhanced, bright, clear, artistic, CGI',
-                     'negative_expansion':'photo, realism, art, painting, drawing, stock photo, photographic, realistic, realism, 35mm film, dslr,, art, drawing, painting, drawing, illustration, anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, impressionist>
+                     'negative_expansion':'photo, realism, art, painting, drawing, stock photo, photographic, realistic, realism, 35mm film, dslr,, art, drawing, painting, drawing, illustration, anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, impressionist'
                    },
           'art':   { 'model':'jzli/realcartoonXL-v6',
                      'pre_prompt': 'drawing of ',
-                     'positive_expansion':'concept art in the style of {watercolor drawing | line art | pencil drawing | crayon art | pastel art | wet paint | digital painting | anime | Storybook Illustration | colored pencil art | Ballpoint Pen Art }, digital artwork, illustrative, p>
+                     'positive_expansion':'concept art in the style of {watercolor drawing | line art | pencil drawing | crayon art | pastel art | wet paint | digital painting | anime | Storybook Illustration | colored pencil art | Ballpoint Pen Art }, digital artwork, illustrative',
                      'negative_expansion':'photo, 3d, cinematic, photography, realism, low contrast, stock photo, photograph, photographic, realistic, 35mm film, dslr'
                    },
           'logo':  { 'model':'stabilityai/stable-diffusion-xl-base-1.0',
