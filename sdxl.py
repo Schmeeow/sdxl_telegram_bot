c21f396b385addce31797e954f262a80375fa6d4
Schmeeow <43118081+Schmeeow@users.noreply.github.com>
2024-02-06 10:07:35 +0300

    Update sdxl.py

--- sdxl.py
+++ sdxl.py
@@ -3,8 +3,10 @@ from diffusers import AutoPipelineForText2Image
 import torch
 import datetime
 
-COMMON_NEGATIVE_PROMPT = 'duplicate, blurry, disfigured, deformed, poorly drawn, extra limbs, watermark, long neck, elongated body, cropped image, deformed hands, twisted fingers, double image, malformed hands, multiple heads, extra limb, ugly, poorly drawn hands, missing limb, lousy'
+# общий негативный промт для всех генераций
+COMMON_NEGATIVE_PROMPT = 'duplicate, blurry, disfigured, deformed, poorly drawn, extra limbs, watermark, long neck, elongated body, cropped image, deformed hands, twisted fingers, double image, malformed hands, multiple heads, extra limb, ugly, poorly drawn hands, missing limb, lousy >
 
+# описание стилей генерации
 STYLES = {'basic': { 'model':'jzli/realcartoonXL-v6',
                      'pre_prompt':'',
                      'positive_expansion':'vibrant, highly detailed, intricate, elegant, sharp, vivid, fine detail, fair quality',
@@ -12,35 +14,31 @@ STYLES = {'basic': { 'model':'jzli/realcartoonXL-v6',
                    },
           'photo': { 'model':'n0madic/colossusProjectXL_v53',
                      'pre_prompt':'photo of ',
-                     'positive_expansion':'photographic, realistic, realism, photography, f/2.8, 35mm photo, highly detailed, cinematic, bokeh, intricate, elegant, sharp, fine detail, aesthetic, pretty, raw photo, photorealistic, 4K, stock photo, photograph, natural soft light, detail',
+                     'positive_expansion':'photographic, realistic, realism, photography, f/2.8, 35mm photo, highly detailed, intricate, cinematic, bokeh, elegant, sharp, fine detail, aesthetic, pretty, raw photo, photorealistic, 4K, stock photo, natural soft light, detailed face, fil>
                      'negative_expansion':'art, drawing, painting, drawing, illustration, anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, impressionist, noisy, blurry'
                    },
           '3d':    { 'model': 'stablediffusionapi/protovisionxl-v3',
                      'pre_prompt':'3d render of ',
-                     'positive_expansion':'3d model, video game character, volumetric light, 4k, active, dynamic, highly detailed, intricate, dramatic light, elegant, sharp, vivid colors, fine detail, fair quality, aesthetic, pretty, attractive, enhanced, bright, clear, artistic, CGI',
-                     'negative_expansion':'photo, realism, art, painting, drawing, stock photo, photographic, realistic, realism, 35mm film, dslr,, art, drawing, painting, drawing, illustration, anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, impressionist'
+                     'positive_expansion':'3d model, video game character, volumetric light, 4k, 3d render, active, dynamic, highly detailed, dimensional, dramatic light, elegant, sharp, vivid colors, fine detail, fair quality, aesthetic, pretty, attractive, enhanced, bright, clear, a>
+                     'negative_expansion':'photo, realism, art, painting, drawing, stock photo, photographic, realistic, realism, 35mm film, art, painting, drawing, illustration, anime, cartoon, graphic, text, crayon, graphite, abstract, glitch, impressionist, noisy, blurry'
                    },
           'art':   { 'model':'jzli/realcartoonXL-v6',
-                     'pre_prompt': 'drawing of ',
-                     'positive_expansion':'concept art in the style of {watercolor drawing | line art | pencil drawing | crayon art | pastel art | wet paint | digital painting | anime | Storybook Illustration | colored pencil art | Ballpoint Pen Art }, digital artwork, illustrative',
-                     'negative_expansion':'photo, 3d, cinematic, photography, realism, low contrast, stock photo, photograph, photographic, realistic, 35mm film, dslr'
+                     'pre_prompt': 'artistic drawing of ',
+                     'positive_expansion':'concept art in the style of {watercolor drawing | line art | pencil drawing | crayon art | pastel art | wet paint | digital painting | anime | Storybook Illustration | colored pencil art | Ballpoint Pen Art }, sketch, digital artwork, illustr>
+                     'negative_expansion':'photo, 3d, cinematic, photography, realism, low contrast, stock photo, photograph, photographic, realistic, 35mm film, dslr, signature, watermark'
                    },
           'logo':  { 'model':'stabilityai/stable-diffusion-xl-base-1.0',
                      'pre_prompt':'logotype of ',
-                     'positive_expansion':'((logo)), mockup, design, (logotype), sign, symbol, original, unique, logo, logotype, minimalistic, vector, flat, clean, simple, modern, white background, color grading, high contrast',
+                     'positive_expansion':'((logo)), schematic, sketch, mockup, design, vector art, (logotype), sign, symbol, original, unique, logo, logotype, minimalistic, vector, flat, clean, simple, modern, white background, color grading, high contrast',
                      'negative_expansion':'photo, realistic, realism, art, cartoon, 3d'
                    }
 }
 
+# возвращает имя сгенерированного файла (image_ID_ПОЛЬЗОВАТЕЛЯ_ВРЕМЯ_ДАТА.jpg)
 def txt2img(user_id, prompt, style):
 
-    prompt = STYLES[style]['pre_prompt'] + prompt + ", "
-
-    print("user id: ", user_id)
-    print("style: ", style)
-    print("model: ", STYLES[style]['model'])
-    print("prompt: ", prompt, STYLES[style]['positive_expansion'])
-    print("negative prompt: ", COMMON_NEGATIVE_PROMPT, STYLES[style]['negative_expansion'], "\n")
+    positive_prompt_final = STYLES[style]['pre_prompt'] + prompt + ", " + STYLES[style]['positive_expansion']
+    negative_prompt_final = COMMON_NEGATIVE_PROMPT + STYLES[style]['negative_expansion']
 
     pipeline_text2image = AutoPipelineForText2Image.from_pretrained(
         STYLES[style]['model'],
@@ -49,9 +47,11 @@ def txt2img(user_id, prompt, style):
     ).to("cuda")
 
     image = pipeline_text2image(
-            prompt = prompt + STYLES[style]['positive_expansion'],
-            negative_prompt = COMMON_NEGATIVE_PROMPT + STYLES[style]['negative_expansion'],
-            num_inference_steps = 60,
+            prompt = positive_prompt_final,
+            negative_prompt = negative_prompt_final,
+            num_inference_steps = 55,
+            use_karras_sigmas=True,
+            original_size = (1024,1024),
             height = 1024,
             width = 1024
     ).images[0]
@@ -59,6 +59,5 @@ def txt2img(user_id, prompt, style):
     image_path = "image_" + user_id + "_" + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + ".jpg"
     image.save(image_path)
 
-    print("filename: ", image_path + "\n")
-
     return(image_path)
+
